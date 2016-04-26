3a072f3e9d9c41ef4997fb3606fe3d27a2e79ce4
saurabhjinturkar <saurabh.jinturkar@sjsu.edu>
2016-04-26 01:35:52 -0700

    Initial project commit

---
+++ ml/src/main/java/in/saurabhjinturkar/ml/App.java
@@ -0,0 +1,13 @@
+package in.saurabhjinturkar.ml;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
